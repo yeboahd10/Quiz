dbc9ab42086bb61b1ead1f460541a878d63fd162
dan <135050270+yeboahd10@users.noreply.github.com>
2023-12-15 07:13:07 +0000

    new

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,8 +1,7 @@
 import React, { useState } from "react";
 import styles from "./Home.module.css";
 import chair from "./raw.png";
-import Profile from "../Profile/Profile";
-import { Route, Routes, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 
 
